9aac64515a6071478cdcc2a8ec1409024907f25d
NatanaelHZ <natanael_henrik28@outlook.com>
2022-07-24 10:51:32 -0300

    Primeiro programa e dicas

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//OU
+/*
+import {
+	"fmt"
+}
+*/
+
+// A porta de entrada de um programa GO
+func main() {
+	fmt.Print("Primeiro...")
+	fmt.Print("Programa")
+
+	/*
+		Sobre comentários
+
+		1) Priorize código legível, comentários somente relevantes
+		2) Evite comentários óbvios
+		3) Durante curso abuse dos comentários, principalmente por novos conceitos
+
+	*/
+}
